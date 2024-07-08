c66967c43a9546aa5c6bae58d67f865d13f54d0f
Ahmed <muhammadahmedhaque@gmail.com>
2024-07-08 22:51:43 +0500

    update controllers of user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -184,10 +184,88 @@ const changePassword = asyncHandle( async (req, res)=>{
         new apiResponse(200,{},"Password changed")
     )
 })
+const getUser = asyncHandle( async (req,res,next) =>{
+    return res
+    .status(200)
+    .json(
+        new apiResponse(200,req.user,"User returned as response")
+    )
+} )
+
+const updateUsernameAndEmail = asyncHandle(async (req,res) =>{
+    const {fullname ,email }=req.body
+    if(!username || !email){
+        return new apiError(400 , "All fields required")
+    }
+    const user =await User.findByIdAndUpdate(req.user._id,
+        {
+            $set:{
+                fullname:fullname, //agar key aur value sem hotu tu sirf aik likh saktay
+                email:email
+            }
+        },
+        {save:true}
+    ).select("-password")
+    return res
+    .status(200)
+    .json(200,user,"email and fullname updated")
+
+})
+const updateAvatar = asyncHandle(async (req,res)=>{
+    const avatarPath = req.file?.avatar.path
+    if(!avatarPath){
+        return new apiError(400,"Avatar path not found")
+    }
+    const avatarURL = await uploadOnCloud(avatarPath)
+    if(!avatarURL.url){
+        return new apiError(400,"Problem in uploading on cloud")
+    }
+    const user = await User.findByIdAndUpdate(req.user?._id,
+        {
+            $set:{ 
+                avatar:avatarURL.url
+            }
+        },
+        {save:true}
+    ).select("-passwaord")
+    return res
+    .status(200)
+    .json(
+        new apiResponse(200,user,"Avatar changed successfully")
+    )
+
+})
+const updateCoverImage = asyncHandle(async (req,res)=>{
+    const coverImagePath = req.file?.avatar.path
+    if(!coverImagePath){
+        return new apiError(400,"Cover image path not found")
+    }
+    const coverImageURL = await uploadOnCloud(coverImagePath)
+    if(!coverImageURL.url){
+        return new apiError(400,"Problem in uploading cover image on cloud")
+    }
+    const user = await User.findByIdAndUpdate(req.user?._id,
+        {
+            $set:{
+                coverImage:coverImageURL.url
+            }
+        },
+        {save:true}
+    ).select("-passwaord")
+    return res
+    .status(200)
+    .json(
+        new apiResponse(200,user,"Cover image changed successfully")
+    )
+    
+})
 
 export {
     registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changePassword,
+    getUser,
+    updateUsernameAndEmail
 }
